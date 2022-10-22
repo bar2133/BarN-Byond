10b9163cc93fca0400ab28392373ce894f41c4c5
bar2133 <83633619+bar2133@users.noreply.github.com>
2022-10-22 14:48:15 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # because i use macos with m1 chip i wrote another automation
 # that install via homebrew pipenv to the machine, sync dependencies
 # and run the django server on local host
-# reccomanded to use with nohup
+# recommended to use with nohup
 
 
 # install pipenv via homebrew package manager
