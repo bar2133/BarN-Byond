857d066a8e4600830d821b2f91e11e32e4a9df88
Bar Nachlieli <bar2133@gmail.com>
2022-10-24 21:11:39 +0300

    add support to macos and fedora on setup.sh and add auto migration on start

--- setup.sh
+++ setup.sh
@@ -3,15 +3,21 @@
 # sync the required dependencies and run the server on localhost with port 8000
 # it is recommended to use the script with nohup  
 
-
-# installing pipenv with homebrew package manager
-brew install pipenv 
-
-# navaigate to the pipenv folder
-cd pipenv 
+if [[ "$OSTYPE" == "darwin"* ]]; then # if the os is macos
+    # installing pipenv with homebrew package manager
+    brew install pipenv 
+    # navaigate to the pipenv folder
+    cd pipenv 
+else
+    sudo -n dnf install -y pipenv
+    cd /pipenv
+fi
 
 # install dependencies with pipenv
 pipenv sync --dev
+
+# Run database migrations
+pipenv run python manage.py migrate
 
 # run the django server in background and save the log to runserver.log
 pipenv run python manage.py runserver 0.0.0.0:8000 > runserver.log 
